8f745661b32ebfce2118d1119ab05b60a29fbf03
tor <student2ua@gmail.com>
2019-10-05 20:49:44 +0300

    sbj-lt-psg->

--- frontend/src/components/SelectVue.vue
+++ frontend/src/components/SelectVue.vue
@@ -7,32 +7,47 @@
         <section v-else>-->
     <div v-if="loading">Loading...</div>
     <div v-else>
-      <b-form-select v-model="selectedSbj" :options="subjects" value-field="first" text-field="second" @change="handleChange">
-        <template v-slot:first>
-          <option :value="null" disabled>-- Выберите один из вариантов --</option>
-        </template>
-      </b-form-select>
-
-  <!--    <select v-model="selectedSbj">
-        <option disabled value="">Выберите один из вариантов</option>
-        <option v-for="iterSbj in subjects" v-bind:value="iterSbj">{{
-          iterSbj.second
-        }}</option>
-      </select>-->
-      <br />
-      <span v-if="selectedSbj !== null">
-     <!--   Выбрано: {{ selectedSbj.first }} - {{ selectedSbj.second }}-->
-        Выбрано: {{ selectedSbj}} - {{ selectedSbj }}
-      </span>
-      <br />
-  <select v-model="selectedLt">
-        <option disabled value="">Выберите один из вариантов</option>
-        <option v-for="iterLt in lessonType" v-bind:value="iterLt">{{
-          iterLt.second
-        }}</option>
-      </select>
+        <b-container fluid>
+            <b-row class="my-1">
+                <b-col sm="2">
+                    <label for="selected-Sbj">Предмет:</label>
+                </b-col>
+                <b-col sm="10">
+                    <b-form-select id="selected-Sbj" v-model="selectedSbj" :options="subjects" value-field="first" text-field="second" @change="handleChangeSbj">
+                        <template v-slot:first>
+                            <option :value="null" disabled>-- Выберите один из вариантов --</option>
+                        </template>
+                    </b-form-select>
+                </b-col>
+            </b-row>
+            <b-row class="my-1">
+                <b-col sm="2">
+                    <label for="selected-LT">LessonType:</label>
+                </b-col>
+                <b-col sm="10">
+                    <div class="mt-2"></div>
+                    <b-form-select id="selected-LT" v-model="selectedLT" :options="lessonType" value-field="first" text-field="second" @change="handleChangeLT">
+                        <template v-slot:first>
+                            <option :value="null" disabled selected>-- Выберите один из вариантов --</option>
+                        </template>
+                    </b-form-select>
+                </b-col>
+            </b-row>
+            <b-row class="my-1">
+                <b-col sm="2">
+                    <label for="selected-PSG">PSG:</label>
+                </b-col>
+                <b-col sm="10">
+                    <div class="mt-2"></div>
+                    <b-form-select id="selected-PSG" v-model="selectedPSG" :options="psgs" value-field="first" text-field="second" @change="handleChangePSG">
+                        <template v-slot:first>
+                            <option :value="null" disabled selected>-- Выберите один из вариантов --</option>
+                        </template>
+                    </b-form-select>
+                </b-col>
+            </b-row>
+        </b-container>
     </div>
-    <!--        </section>-->
   </div>
 </template>
 <script>
@@ -47,6 +62,9 @@ export default {
     selectedSbj: null,
     lessonType: [],
     selectedLt: null,
+      psgs: [],
+    selectedPSG: null,
+      handsontable:null,
     loading: true
   }),
   created() {
@@ -66,21 +84,9 @@ export default {
       })
       .finally(() => (self.loading = false));
   },
-    computed:{
-         getSubjects:function () {
-             const header = { Authorization: "Bearer " + this.$store.getters.getToken };
-            return AXIOS.get(`${BASE_URL}/subjects`, { headers: header });
-
-         },
-        getLT:function () {
-           if (!this.selectedSbj) return ;
-            const header = { Authorization: "Bearer " + this.$store.getters.getToken };
-            return AXIOS.get(`${BASE_URL}/subjects/${this.selectedSbj}/lessontypes`, { headers: header });
-        }
-    },
-  methods: {
-      handleChange: function (e) {
-          console.log(e);
+   methods: {
+      handleChangeSbj: function (e) {
+          console.log('handleChangeSbj'+e);
           let self = this;
           this.lessonType=[];
          /* this.lessonType.push(
@@ -93,6 +99,42 @@ export default {
                       JSON.stringify(response.data, null, "\t")
                   );
                   self.lessonType = response.data;
+              })
+              .catch(e => {
+                  self.errors.push(e);
+              })
+              .finally(() => (self.loading = false));
+
+      },
+      handleChangeLT: function (e) {
+          console.log('handleChangeLT '+e);
+          let self = this;
+          this.psgs=[];
+          AXIOS.get(`${BASE_URL}/subjects/${self.selectedSbj}/lessontypes/${e}/psgs`, { headers: { Authorization: "Bearer " + this.$store.getters.getToken }  })
+              .then(function(response) {
+                  console.log(
+                      `GET: Here's the list of PSG`,
+                      JSON.stringify(response.data, null, "\t")
+                  );
+                  self.psgs = response.data;
+              })
+              .catch(e => {
+                  self.errors.push(e);
+              })
+              .finally(() => (self.loading = false));
+
+      },
+      handleChangePSG: function (e) {
+          console.log('handleChangeLT '+e);
+          let self = this;
+          this.handsontable=null;
+          AXIOS.get(`${BASE_URL}/subjects/${self.selectedSbj}/lessontypes/${self.selectedPSG}/psgs/${e}/handsontable`, { headers: { Authorization: "Bearer " + this.$store.getters.getToken }  })
+              .then(function(response) {
+                  console.log(
+                      `GET: Here's the list of HandsonTable`,
+                      JSON.stringify(response.data, null, "\t")
+                  );
+                  self.handsontable = response.data;
               })
               .catch(e => {
                   self.errors.push(e);
@@ -124,7 +166,17 @@ export default {
           self.errors.push(e);
         });
       // this.$bar.finish();
-    }
+    }   ,
+      getSubjects:function () {
+          const header = { Authorization: "Bearer " + this.$store.getters.getToken };
+          return AXIOS.get(`${BASE_URL}/subjects`, { headers: header });
+
+      },
+      getLT:function () {
+          if (!this.selectedSbj) return ;
+          const header = { Authorization: "Bearer " + this.$store.getters.getToken };
+          return AXIOS.get(`${BASE_URL}/subjects/${this.selectedSbj}/lessontypes`, { headers: header });
+      }
   }
 };
 </script>
