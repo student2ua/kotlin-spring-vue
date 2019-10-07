39281b28606a2fd00dc4d0415fd7cad1cb25c333
tor <student2ua@gmail.com>
2019-10-07 21:17:18 +0300

    test data

--- backend/src/main/kotlin/com/tor/kotlin/spring/backend/repo/MarkRESTRepositoryImpl.kt
+++ backend/src/main/kotlin/com/tor/kotlin/spring/backend/repo/MarkRESTRepositoryImpl.kt
@@ -1,22 +1,41 @@
 package com.tor.kotlin.spring.backend.repo
 
-import org.springframework.context.annotation.Bean
 import java.util.*
 
 class MarkRESTRepositoryImpl : MarkRESTRepository {
-    override fun getHandsontable(humanId: Int, subjId: Int, lessonTypeId: Int, psgId: Int): String =
-        this::class.java.classLoader.getResource("handsontable.json").readText()
+    val pz = Pair(1, "Pz")
+    val lecture = Pair(2, "Lecture")
+    val lab = Pair(3, "Lab")
 
-
-    override fun getSubjects(humanId: Int): List<Pair<Int, String>> =
-            Arrays.asList(Pair(1, "test"))
-
-    override fun getLessonTyps(humanId: Int, subjId: Int): List<Pair<Int, String>> =
-            Arrays.asList(Pair(2, "testLT"))
-
-    override fun getPSGs(humanId: Int, subjId: Int, lessonTypeId: Int): List<Pair<Int, String>> =
-            Arrays.asList(
-            Pair(3, "testPSG_1"),
-            Pair(4, "testPSG_2")
+    var dataMap: Map<Pair<Int, String>, List<Pair<Int, String>>> = mapOf(
+            (Pair(1, "Хвостопадство") to Arrays.asList(pz)),
+            (Pair(2, "Хвосторубство") to Arrays.asList(lecture, pz)),
+            (Pair(3, "Зельеварение") to Arrays.asList(lecture, lab, pz))
     )
+
+    override fun getHandsontable(humanId: Int, subjId: Int, lessonTypeId: Int, psgId: Int): String =
+            this::class.java.classLoader.getResource("handsontable.json").readText()
+
+
+    override fun getSubjects(humanId: Int): List<Pair<Int, String>> = dataMap.keys.toList().sortedBy { it.second }
+//            Arrays.asList(Pair(1, "SubjectTest"))
+
+    override fun getLessonTyps(humanId: Int, subjId: Int): List<Pair<Int, String>> = dataMap.entries
+            .filter { it.key.first == subjId }
+            .flatMap { it.value }
+            .sortedBy { it.second }
+//            Arrays.asList(Pair(2, "testLT"))
+
+    override fun getPSGs(humanId: Int, subjId: Int, lessonTypeId: Int): List<Pair<Int, String>> {
+        val sliz = Pair(1, "Слизерин")
+        val griff = Pair(2, "Гриффиндор")
+        val puf = Pair(3, "Пуффендуй")
+        val kog = Pair(4, "Когтевран")
+        
+        return when (Pair(subjId, lessonTypeId)) {
+            Pair(1, 1) -> Arrays.asList(sliz, griff)
+            Pair(2, 1), Pair(2, 2) -> Arrays.asList(sliz, puf, kog)
+            else -> Arrays.asList(sliz, griff, puf, kog)
+        }.sortedBy { it.second }
+    }
 }
